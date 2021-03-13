4f2b131d76d45925325ef45ba3c24f397ecd77b6
Eric Horne <echorne@uncc.edu>
2021-03-13 18:32:47 -0500

    I got the sql insert working on my laptop :)

--- echorne/add_test.py
+++ echorne/add_test.py
@@ -11,10 +11,10 @@ def add_1000(filenames, df):
     '''It doesn't really have to be 1000. \
     The dataframe length can be longer than the filenames length.'''
     cnx = getConnection()
-    cursor = cnx.connect()
+    cursor = cnx.cursor()
     for file in filenames:
         try:
-            insertTrain(cursor, file, df.loc[file[:-4]]['instrument'])
+            insertTrain(cursor, file, df.loc[file[:-4]])
         except Exception as e:
             message = 'Could not retrieve %s from json file dataframe.' % e
             print(message)
@@ -25,11 +25,16 @@ def add_1000(filenames, df):
 def insertTrain(cursor, filename, target, fft='', mfcc='', folder_id=1):
     '''Just insert training data into the train table.'''
     add_train = ("INSERT INTO train "
-                 "(%(fft, mfcc, filename, folder_id, target)s) "
+                 "(fft, mfcc, filename, folder_id, target) "
                  "VALUES (%(fft)s, %(mfcc)s, %(filename)s, %(folder_id)s, %(target)s)")
     data_train = {'filename': filename, 'target': target,
                   'fft': fft, 'mfcc': mfcc, 'folder_id': folder_id}
+    err = 0
+    exceptions = 0
     try:
         cursor.execute(add_train, data_train)
     except Exception as e:
-        print(e)
+        err += 1
+        exceptions = e
+    if err:
+        print('errors "%s": %d' % (exceptions, err))
--- echorne/get_test.py
+++ echorne/get_test.py
@@ -5,9 +5,10 @@ import add_test
 audioFolder = path.join(getcwd(), 'audio')
 _, _, musicFiles = next(walk(audioFolder))
 
-examples = pd.read_json('examples.json').transpose().loc[:, 'instrument']
+examples = pd.DataFrame(pd.read_json(
+    'examples.json').transpose().loc[:, 'instrument'])
 
-for i in range((examples.shape[0] % 1000)+1):
+for i in range((examples.shape[0] // 1000)+1):
     realIndex = i*1000
     maxIndex = (realIndex+1000) if realIndex + \
         1000 <= len(musicFiles) else len(musicFiles)
