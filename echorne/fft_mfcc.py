c2bbc4517bdff6dc8b25cefed1f5b8314a917fb3
Eric Horne <echorne@uncc.edu>
2021-03-13 22:00:32 -0500

    Making a little adjustment to print a more accurate string representation that can be tweaked precisely

--- echorne/fft_mfcc.py
+++ echorne/fft_mfcc.py
@@ -6,4 +6,8 @@ def getFftMfcc(wav):
     y, sr = librosa.load(wav, mono=True)
     fft = np.fft.fft(y)
     mfcc = librosa.feature.mfcc(y=y, sr=sr)
-    return str(fft), str(mfcc)
+    fftStr = np.array2string(
+        fft, precision=2, separator=',', suppress_small=True)
+    mfccStr = np.array2string(
+        mfcc, precision=2, separator=',', suppress_small=True)
+    return fftStr, mfccStr
