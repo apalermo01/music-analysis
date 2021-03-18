b9e8feef7e9505685547b1f98ac8a45973d6450d
apalermo01 <64085614+apalermo01@users.noreply.github.com>
2021-03-17 20:08:16 -0400

    tried to resolve inconsistencies to allow merge

--- Sandbox/echorne/add_test.py
+++ Sandbox/echorne/add_test.py
@@ -3,6 +3,7 @@ import mysql.connector
 import fft_mfcc
 from os import path
 
+TABLE = 'test'
 
 def getConnection():
     return mysql.connector.connect(
@@ -20,7 +21,7 @@ def add_1000(filenames, df, relativePath=''):
             wav = path.join(relativePath, filename)
         try:
             mfcc = fft_mfcc.getFftMfcc(wav)
-            insertTrain(cursor, filename, df.loc[filename[:-4]], None, mfcc)
+            insertTest(cursor, filename, df.loc[filename[:-4]], None, mfcc)
         except Exception as e:
             message = 'Could not retrieve %s from json file dataframe.' % e
             print(message)
@@ -28,17 +29,17 @@ def add_1000(filenames, df, relativePath=''):
     cnx.close()
 
 
-def insertTrain(cursor, filename, target, fft='', mfcc='', folder_id=1):
-    '''Just insert training data into the train table.'''
-    add_train = ("INSERT INTO train "
+def insertTest(cursor, filename, target, fft='', mfcc='', folder_id=1):
+    '''Just insert testing data into the test table.'''
+    add_test = (f"INSERT INTO {TABLE} "
                  "(fft, mfcc, filename, folder_id, target) "
                  "VALUES (%(fft)s, %(mfcc)s, %(filename)s, %(folder_id)s, %(target)s)")
-    data_train = {'filename': filename, 'target': target,
+    data_test = {'filename': filename, 'target': target,
                   'fft': fft, 'mfcc': mfcc, 'folder_id': folder_id}
     err = 0
     exceptions = 0
     try:
-        cursor.execute(add_train, data_train)
+        cursor.execute(add_test, data_test)
     except Exception as e:
         err += 1
         exceptions = e
